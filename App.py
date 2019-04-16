c3ea014fe0fc1cea3d26a312862947b7b563f147
yuzhou346694246 <442704997@qq.com>
2019-04-16 19:37:28 +0800

    second

--- App.py
+++ App.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask,request,url_for,render_template
 
 app = Flask(__name__)
 
+#url_for('static',fi)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    return "Hello Dongge!"
+    #return "Hello Dongge!"
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        pass
