377b92469487f12fbaead41c401623b0a3d60a2f
unknown <yuzhou346694246@126.com>
2019-04-18 11:22:33 +0800

    dd

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,url_for,render_template, send_file
+from flask import Flask,request,url_for,render_template, send_file, redirect
 #from __future__ import unicode_literals
 import youtube_dl
 import os
@@ -30,6 +30,22 @@ def downloadList():
     tree = make_tree('./downloads')
     return render_template('list.html', tree=tree)
 
+@app.route("/delete")
+def delete():
+    '''
+    /delete?hash=xxx
+    '''
+    arg = request.args.get('hash')
+    hashinfo = gethashinfo(arg)
+    if hashinfo == None:
+        return "<h1>文件不存在</h1>"
+    path = hashinfo['path']
+    try:
+        remove(path)
+        return redirect("/list")
+    except FileNotFoundError:
+        return "<h1>文件不存在</h1>"
+
 
 @app.route('/return-file', methods = ['GET'])
 def returnFile():
@@ -56,14 +72,14 @@ def returnFile():
 ####################以下为功能函数
 def download(url):
     ydl_opts = {
-        'format':'mp4',
+        'format':'bestvideo',
         'outtmpl':'./downloads/%(title)s.%(ext)s'
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 def make_tree(path):
-    tree = dict(name=os.path.basename(path),isdir=True, children=[])
+    tree = dict(name=os.path.basename(path),isdir=True, children=[],hashvalue=filename2hash(path))
     try: lst = os.listdir(path)
     except OSError:
         pass #ignore errors
@@ -105,6 +121,8 @@ def init_hashfilenames():
         for filename in filenames:
             tpath = os.path.join(path,filename)
             if os.path.isdir(tpath):
+                h = filename2hash(tpath)
+                hashfilenames[h] = { 'filename':filename,'path':tpath}
                 listdir(tpath)
             else:
                 h = filename2hash(tpath)
@@ -119,3 +137,23 @@ def gethashinfo(h):
     except KeyError:
         return None
                 
+
+def remove(path):
+    '''
+    采用递归的方式删除文件或目录
+    原因：如果目录不为空，那么rmdir就会报错
+    '''
+    def t_remove(path):
+        if os.path.isdir(path):
+            for filename in os.listdir(path):
+                t_path = os.path.join(path,filename)
+                # if os.path.isdir(t_path):
+                #     t_remove(path)
+                #     os.rmdir(t_path)
+                # else:
+                #     os.remove(t_path)
+                t_remove(t_path)
+            os.rmdir(path)
+        else:
+            os.remove(path)
+    t_remove(path)
